68044c5d79e16314a390e6eaa31b309732cdb3ef
kub22848-dev <kub22848@gmail.com>
2026-09-12 20:17:14 +0300

    Update lesson_1.sql

--- lesson_1.sql
+++ lesson_1.sql
@@ -12,3 +12,22 @@ SELECT price FROM users;
 
 DROP TABLE users;
 
+CREATE TABLE Trip(
+    Company,
+	id INT(20),
+  	name VARCHAR(20),
+	age INT(20)
+);
+
+
+insert into Company VALUES(1, 'Don_avia', 20);
+INSERT INTO Company VALUES(2, 'Aeroflot', 17);
+insert into Company VALUES(3, 'Dale_avia', 18);
+insert into Company VALUES(4, 'air_France', 32);
+insert into Company VALUES(5, 'British_AW', 15);
+
+SELECT * FROM Company;                  
+SELECT name, age FROM Company WHERE age > 18;
+
+DROP TABLE Company;
+DROP TABLE Trip;
